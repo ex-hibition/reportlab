f6c65c0e989d9fcb49777525964cce830774a7b2
ex-hibition <nozomumumu@gmail.com>
2018-03-16 01:27:24 +0900

    日本表示対応

--- createpdf.py
+++ createpdf.py
@@ -1,6 +1,3 @@
-# Sample platypus document
-# From the FAQ at reportlab.org/oss/rl-toolkit/faq/#1.1
-
 from reportlab.platypus import SimpleDocTemplate, Paragraph, Spacer
 from reportlab.lib.styles import getSampleStyleSheet
 from reportlab.rl_config import defaultPageSize
@@ -18,24 +15,25 @@ class CreatePDF:
     
     PAGE_HEIGHT=defaultPageSize[1]
     PAGE_WIDTH=defaultPageSize[0]
+
     styles = getSampleStyleSheet()
-    
+    this_styles = styles["Normal"]
+    this_styles.fontName = "HeiseiMin-W3"
+
     Title = u"申込内容通知サンプル:title"
     pageinfo = u"申込内容通知サンプル:platypus example"
     
+    # -- 1ページ目専用 
     def myFirstPage(self, canvas, doc):
         canvas.saveState()
         canvas.setFont('HeiseiMin-W3', 16)
         canvas.drawCentredString(self.PAGE_WIDTH/2.0, self.PAGE_HEIGHT-30, self.Title)
 
         canvas.setFont('HeiseiMin-W3', 9)
-
-
-
-        canvas.setFont('HeiseiMin-W3', 9)
         canvas.drawString(cm, 0.75 * cm,"Page %d %s" % (doc.page, self.pageinfo))
         canvas.restoreState()
     
+    # -- 2ページ目以降
     def myLaterPages(self, canvas, doc):
         canvas.saveState()
         canvas.setFont('HeiseiMin-W3', 9)
@@ -44,24 +42,31 @@ class CreatePDF:
     
     def go(self, order):
         doc = SimpleDocTemplate(f"skit_sample_{order['id']}.pdf")
-        Story = [Spacer(1,2*cm)]
-        style = self.styles["Normal"]
-        for i in range(100):
-            p = Paragraph(order['id'], style)
-            Story.append(p)
-            Story.append(Spacer(1,0.2*cm))
 
-            address = order['address_1'] + order['address_2'] + order['address_3']
-            p = Paragraph(unicode(address, 'utf-8'), style)
-            Story.append(p)
-            Story.append(Spacer(1,0.2*cm))
+        story = [Spacer(1,2*cm)]
+        style = self.this_styles
 
-            bogustext = ("Paragraph number %s. " % i) *5
-            p = Paragraph(bogustext, style)
-            Story.append(p)
-            Story.append(Spacer(1,0.2*cm))
+        p = Paragraph(order['id'], style)
+        story.append(p)
+        story.append(Spacer(1,0.5*cm))
 
-        doc.build(Story, onFirstPage=self.myFirstPage, onLaterPages=self.myLaterPages)
+        address = order['address_1'] + order['address_2'] + order['address_3'] + order['address_name']
+        p = Paragraph(address, style)
+        story.append(p)
+        story.append(Spacer(1,0.5*cm))
+
+        details = order['order_date'] + order['order_no'] + order['order_plan']
+        p = Paragraph(details, style)
+        story.append(p)
+        story.append(Spacer(1,0.5*cm))
+
+        for i in range(5):
+            caution = order['quality'] * 20
+            p = Paragraph(caution, style)
+            story.append(p)
+            story.append(Spacer(1,0.5*cm))
+
+        doc.build(story, onFirstPage=self.myFirstPage, onLaterPages=self.myLaterPages)
     
 
 import pandas as pd
